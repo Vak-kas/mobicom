586ba49742049feb7e1b93ea1d4eef0d57e9c252
서민재 <hey_minj@naver.com>
2024-04-26 09:46:28 +0900

    7주차 과제

--- week7_hw2/app/src/main/java/com/example/week7_hw2/MainActivity.java
+++ week7_hw2/app/src/main/java/com/example/week7_hw2/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.week7_hw2;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.TextView;
 
+import com.example.week7_hw2.R;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    boolean login = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
+                        .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -48,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+
     }
 
     @Override
@@ -56,6 +72,99 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.login) {
+            if (login) {
+                // 이미 로그인되어 있으면 로그아웃 처리
+                login = false;
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("사용자 입력");
+
+
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_signin, null);
+                builder.setView(dialogView);
+
+
+                EditText nameEditText = dialogView.findViewById(R.id.name);
+                EditText emailEditText = dialogView.findViewById(R.id.email);
+                RadioGroup idolRadioGroup = dialogView.findViewById(R.id.idol);
+                NavigationView navigationView = findViewById(R.id.nav_view);
+                View headerView = navigationView.getHeaderView(0);
+                TextView textViewName = headerView.findViewById(R.id.textViewName);
+                TextView textViewEmail = headerView.findViewById(R.id.textViewEmail);
+                textViewName.setText("Android Studio");
+                textViewEmail.setText("android.studio@android.com");
+                ImageView imageView = headerView.findViewById(R.id.imageView);
+                imageView.setImageResource(R.mipmap.ic_launcher_round);
+
+
+                Toast.makeText(this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("사용자 입력");
+
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_signin, null);
+                builder.setView(dialogView);
+
+                EditText nameEditText = dialogView.findViewById(R.id.name);
+                EditText emailEditText = dialogView.findViewById(R.id.email);
+                RadioGroup idolRadioGroup = dialogView.findViewById(R.id.idol);
+                NavigationView navigationView = findViewById(R.id.nav_view);
+                View headerView = navigationView.getHeaderView(0);
+                TextView textViewName = headerView.findViewById(R.id.textViewName);
+                TextView textViewEmail = headerView.findViewById(R.id.textViewEmail);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String enteredName = nameEditText.getText().toString();
+                        String enteredEmail = emailEditText.getText().toString();
+
+                        textViewName.setText(enteredName);
+                        textViewEmail.setText(enteredEmail);
+
+
+                        int selectedId = idolRadioGroup.getCheckedRadioButtonId();
+                        RadioButton selectedRadioButton = dialogView.findViewById(selectedId);
+
+
+                        ImageView imageView = headerView.findViewById(R.id.imageView);
+                        if (selectedRadioButton.getId() == R.id.an) {
+                            imageView.setImageResource(R.drawable.an);
+                        } else if (selectedRadioButton.getId() == R.id.kim) {
+                            imageView.setImageResource(R.drawable.kim);
+                        } else if (selectedRadioButton.getId() == R.id.jang) {
+                            imageView.setImageResource(R.drawable.jang);
+                        }
+
+                        login = true;
+                    }
+                });
+
+                // "Cancel" 버튼 클릭 시 처리
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "cancel action", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem loginItem = menu.findItem(R.id.login);
+        if (login) {
+            loginItem.setTitle("로그아웃");
+        } else {
+            loginItem.setTitle("로그인");
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
--- week7_hw2/app/src/main/java/com/example/week7_hw2/ui/gallery/GalleryFragment.java
+++ week7_hw2/app/src/main/java/com/example/week7_hw2/ui/gallery/GalleryFragment.java
@@ -1,71 +1,32 @@
 package com.example.week7_hw2.ui.gallery;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.week7_hw2.R;
 import com.example.week7_hw2.databinding.FragmentGalleryBinding;
-import com.example.week7_hw2.databinding.FragmentSlideshowBinding;
-import com.example.week7_hw2.ui.slideshow.SlideshowViewModel;
-import com.google.android.material.navigation.NavigationView;
+import com.example.week7_hw2.ui.gallery.GalleryViewModel;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
+    private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-
-        android.app.AlertDialog.Builder dlg = new AlertDialog.Builder(getActivity());
-
-        dlg.setTitle("좋아하는 버전은?");
-        dlg.setIcon(R.mipmap.ic_launcher);
-
-
-        View dialogView;
-
-        dialogView = (View) View.inflate(getActivity(), R.layout.dialog_signin, null);
-        dlg.setTitle("사용자 입력");
-        dlg.setIcon(R.mipmap.ic_launcher);
-
-        dlg.setView(dialogView);
-
-        dlg.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-
-
-            }
-        });
-        dlg.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-
-
-            }
-        });
-
-        dlg.show();
+        final TextView textView = binding.textGallery;
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
